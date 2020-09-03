8d6adad59e4513dae1ca670bbdcc810712a5fb01
Jack Kingham <kinghamjack12@gmail.com>
2020-09-04 06:11:25 +1000

    Project Initiated

--- app/config/database/Database.php
+++ app/config/database/Database.php
@@ -1,3 +1,44 @@
 <?php
 
-// Database Base Class goes here.
+namespace App\Config\Database;
+
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+class Database {
+    private $host = 'localhost';
+    private $database = 'database';
+    private $username;
+    private $password;
+    private $dsn;
+    public $results = array();
+
+    function __construct() {
+        $this->username = $_ENV['USERNAME'];
+        $this->password = $_ENV['PASSWORD'];
+
+        try {
+            $this->dsn = new \PDO('mysql:host=' . $this->host . ';dbname=' . $this->database . ';charset=utf8;', $this->username, $this->password);
+        } catch(Exception $error) {
+            echo $error->getMessage();
+        }
+    }
+
+    function connect(string $sql, array $params = array()) : array {
+        $count = 1;
+        $stmt = $this->dsn->prepare($sql);
+
+        foreach($params as $param) {
+            $stmt->bindValue($count, $param);
+            $count++;
+        }
+
+        $stmt->execute();
+
+        while($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+            array_push($this->results, $row);
+        }
+
+        return $this->results;
+    }
+}
